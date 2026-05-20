a90766ac62c4884a1ff1eb117a261d02de8183ae
ABDELLAOUI Myriam <12402411@r20314.iutv.univ-paris13.fr>
2026-05-20 10:18:30 +0200

    testVoiture

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -11,6 +11,9 @@ public class VoitureTest {
 
     @Test
     void creerVoiture(){
+        Voiture voiture = new Voiture("Opel",2000);
+        Assert.isTrue(voiture.getMarque().equals("Opel"),"Doit être Opel");
+        Assert.isTrue(voiture.getPrix().equals()==2000,"Doit être 2000");
         assertEquals(1,1);
     }
 
